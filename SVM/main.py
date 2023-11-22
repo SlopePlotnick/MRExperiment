878090d2902e016ac67a0dc185b3e02772efb460
Rzy <815459626@qq.com>
2023-11-22 14:54:12 +0800

    修改easy.py执行结果

--- SVM/main.py
+++ SVM/main.py
@@ -8,45 +8,45 @@ import matplotlib.pyplot as plt
 plt.rcParams["font.sans-serif"] = ["Hiragino Sans GB"] #解决中文字符乱码的问题
 plt.rcParams["axes.unicode_minus"] = False #正常显示负号
 
-# #-----任务1-----
-# train_label_1,train_pixel_1 = svm_read_problem('svmguide1.txt')
-# predict_label_1,predict_pixel_1 = svm_read_problem('svmguide1_test.txt')
-# m1 = svm_train(train_label_1, train_pixel_1)
-# print("#1 result:")
-# p_label_1, p_acc_1, p_val_1 = svm_predict(predict_label_1, predict_pixel_1, m1);
-# print(p_acc_1)
-#
-# #-----任务2-----
-# train_label_2,train_pixel_2 = svm_read_problem('scaledata.txt')
-# predict_label_2,predict_pixel_2 = svm_read_problem('scaledata_test.txt')
-# m2 = svm_train(train_label_2, train_pixel_2)
-# print("#2 result:")
-# p_label_2, p_acc_2, p_val_2 = svm_predict(predict_label_2, predict_pixel_2, m2);
-# print(p_acc_2)
-#
-# #-----任务3-----
-# train_label_3,train_pixel_3 = svm_read_problem('svmguide1.txt')
-# predict_label_3,predict_pixel_3 = svm_read_problem('svmguide1_test.txt')
-# m3 = svm_train(train_label_3, train_pixel_3, '-t 0')
-# print("#3 result:")
-# p_label_3, p_acc_3, p_val_3 = svm_predict(predict_label_3, predict_pixel_3, m3);
-# print(p_acc_3)
-#
-# #-----任务4-----
-# train_label_4,train_pixel_4 = svm_read_problem('svmguide1.txt')
-# predict_label_4,predict_pixel_4 = svm_read_problem('svmguide1_test.txt')
-# m4 = svm_train(train_label_4, train_pixel_4, '-c 1000 -t 2')
-# print("#4 result:")
-# p_label_4, p_acc_4, p_val_4 = svm_predict(predict_label_4, predict_pixel_4, m4);
-# print(p_acc_4)
-#
-# #-----任务5-----
-# train_label_5,train_pixel_5 = svm_read_problem('scaledata.txt')
-# predict_label_5,predict_pixel_5 = svm_read_problem('scaledata_test.txt')
-# m5 = svm_train(train_label_5, train_pixel_5, '-c 8 -g 2 -t 2')
-# print("#5 result:")
-# p_label_5, p_acc_5, p_val_5 = svm_predict(predict_label_5, predict_pixel_5, m5);
-# print(p_acc_5)
+#-----任务1-----
+train_label_1,train_pixel_1 = svm_read_problem('svmguide1.txt')
+predict_label_1,predict_pixel_1 = svm_read_problem('svmguide1_test.txt')
+m1 = svm_train(train_label_1, train_pixel_1)
+print("#1 result:")
+p_label_1, p_acc_1, p_val_1 = svm_predict(predict_label_1, predict_pixel_1, m1);
+print(p_acc_1)
+
+#-----任务2-----
+train_label_2,train_pixel_2 = svm_read_problem('scaledata.txt')
+predict_label_2,predict_pixel_2 = svm_read_problem('scaledata_test.txt')
+m2 = svm_train(train_label_2, train_pixel_2)
+print("#2 result:")
+p_label_2, p_acc_2, p_val_2 = svm_predict(predict_label_2, predict_pixel_2, m2);
+print(p_acc_2)
+
+#-----任务3-----
+train_label_3,train_pixel_3 = svm_read_problem('svmguide1.txt')
+predict_label_3,predict_pixel_3 = svm_read_problem('svmguide1_test.txt')
+m3 = svm_train(train_label_3, train_pixel_3, '-t 0')
+print("#3 result:")
+p_label_3, p_acc_3, p_val_3 = svm_predict(predict_label_3, predict_pixel_3, m3);
+print(p_acc_3)
+
+#-----任务4-----
+train_label_4,train_pixel_4 = svm_read_problem('svmguide1.txt')
+predict_label_4,predict_pixel_4 = svm_read_problem('svmguide1_test.txt')
+m4 = svm_train(train_label_4, train_pixel_4, '-c 1000 -t 2')
+print("#4 result:")
+p_label_4, p_acc_4, p_val_4 = svm_predict(predict_label_4, predict_pixel_4, m4);
+print(p_acc_4)
+
+#-----任务5-----
+train_label_5,train_pixel_5 = svm_read_problem('scaledata.txt')
+predict_label_5,predict_pixel_5 = svm_read_problem('scaledata_test.txt')
+m5 = svm_train(train_label_5, train_pixel_5, '-c 8192 -g 0.03125 -t 2')
+print("#5 result:")
+p_label_5, p_acc_5, p_val_5 = svm_predict(predict_label_5, predict_pixel_5, m5);
+print(p_acc_5)
 
 #-----任务6-----
 train_label_6,train_pixel_6 = svm_read_problem('w1a.txt')
